d74d55c259295f16dbbbc8bab57c52f3f42dd018
Safiya Elmi <240500598@mycput.ac.za>
2026-08-21 21:39:45 +0200

    Updated user service classes

--- src/main/java/za/ac/cput/project3assignment/service/IUserService.java
+++ src/main/java/za/ac/cput/project3assignment/service/IUserService.java
@@ -1,4 +1,19 @@
 package za.ac.cput.project3assignment.service;
 
+import za.ac.cput.project3assignment.domain.User;
+
+import java.util.List;
+
+/* Safiya Abdulkadir Elmi
+240500598
+User service class
+21/08/2026
+ */
+
 public interface IUserService {
+    User create(User user);
+    User read(String userId);
+    List<User> getAll();
+    User update(User user);
+    void delete(String userId);
 }
